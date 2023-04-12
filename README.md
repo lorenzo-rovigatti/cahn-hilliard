f918d98261c77665c3855a8be39bb22f7859780e
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2023-04-12 15:45:28 +0200

    Use CMake to compile the code

--- README.md
+++ README.md
@@ -6,18 +6,29 @@ $$
 f_{\rm bulk}(\psi) = -\frac{1}{2}\epsilon \psi^2 + \frac{1}{4} \psi^4
 $$
 
-where $\epsilon = (T_c - T) / T_c$ and $\psi$ is the order parameter of the phase transition. The total free-energy density used in the code is thus
+where $\epsilon = (T_c - T) / T_c$ and $\psi$ is the order parameter of the phase transition. The total free-energy density used in the code is
 
 $$
-f(\psi) = -\frac{1}{2}\epsilon \psi^2 + \frac{1}{4} \psi^4 + \frac{1}{2} |\nabla \psi|^2
+f(\psi) = -\frac{1}{2}\epsilon \psi^2 + \frac{1}{4} \psi^4 + \frac{1}{2} \kappa |\nabla \psi|^2
 $$
 
-## Compilation and usage
+where $\kappa$ defaults to one.
 
-Use `compile.sh`, which assumes that g++ is installed and in the PATH, to compile the code, and run it with
+## Compilation
+
+Compilation requires CMake. Follow these steps to compile the code:
+
+1. `mkdir build`
+2. `cd build`
+3. `cmake ..`
+4. `make`
+
+At the end of the process an output executable, `ch`, will be placed in the `build` folder.
+
+## Usage
 
 ```
-$ ./ch <N> <epsilon> <psi_average> <steps>
+$ ./ch N epsilon psi_average steps [k=1.0]
 ```
 
 where
@@ -26,5 +37,6 @@ where
 * `epsilon` is the parameter controlling the distance from the critical point (see above). Phase separation requires $\epsilon > 0$. The larger the number, the farther from the critical point.
 * `psi_average` controls the initial conditions of the grid. Each cell of the grid will be assigned a random value that is `psi_average` $\pm 0.5$.
 * `steps` is the number of iterations that the simulation will run for.
+* `k`, which defaults to 1, controls the width of the interfaces between the domains.
 
 At the end of the simulation the final grid will be printed to the file `last.dat`.
--- ch.cpp
+++ ch.cpp
@@ -12,142 +12,152 @@
 
 template<int dims>
 struct CahnHilliard {
-  int N = 64;
-  int N_minus_one;
-  int bits;
-  int size;
-  double epsilon;
+	int N = 64;
+	int N_minus_one;
+	int bits;
+	int size;
+	double epsilon, k_laplacian;
 
-  std::vector<double> psi;
+	std::vector<double> psi;
 
-  CahnHilliard(int mN, double eps, double psi_average) : N(mN), epsilon(eps) {
-    double log2N = std::log2(N);
-    if(ceil(log2N) != floor(log2N)) {
-      fprintf(stderr, "N should be a power of 2\n");
-      exit(1);
-    }
+	CahnHilliard(int mN, double eps, double psi_average, double k) :
+					N(mN),
+					epsilon(eps),
+					k_laplacian(k) {
+		double log2N = std::log2(N);
+		if(ceil(log2N) != floor(log2N)) {
+			fprintf(stderr, "N should be a power of 2\n");
+			exit(1);
+		}
 
-    N_minus_one = N - 1;
-    bits = (int) log2N;
-    
-    size = N;
-    for(int i = 1; i < dims; i++) {
-      size *= N;
-    }
+		N_minus_one = N - 1;
+		bits = (int) log2N;
 
-    psi.resize(size);
-    std::generate(psi.begin(), psi.end(), [psi_average]() {return (drand48() - 0.5) + psi_average;});
-  }
+		size = N;
+		for(int i = 1; i < dims; i++) {
+			size *= N;
+		}
 
-  void fill_coords(int coords[dims], int idx);
-  int cell_idx(int coords[dims]);
+		psi.resize(size);
+		std::generate(psi.begin(), psi.end(), [psi_average]() {
+			return (drand48() - 0.5) + psi_average;
+		});
+	}
 
-  double cell_laplacian(std::vector<double> &field, int idx);
+	void fill_coords(int coords[dims], int idx);
+	int cell_idx(int coords[dims]);
 
-  void evolve();
+	double cell_laplacian(std::vector<double> &field, int idx);
 
-  void print_state(std::string filename);
+	void evolve();
+
+	void print_state(std::string filename);
 };
 
 template<int dims>
 void CahnHilliard<dims>::fill_coords(int coords[dims], int idx) {
-  for(int d = 0; d < dims; d++) {
-    coords[d] = idx & N_minus_one;
-    idx >>= bits; // divide by N
-  }
+	for(int d = 0; d < dims; d++) {
+		coords[d] = idx & N_minus_one;
+		idx >>= bits; // divide by N
+	}
 }
 
 template<int dims>
 int CahnHilliard<dims>::cell_idx(int coords[dims]) {
-  int idx = 0;
-  int multiply_by = 1;
-  for(int d = 0; d < dims; d++) {
-    idx += coords[d] * multiply_by;
-    multiply_by <<= bits; // multiply by N
-  }
-  return idx;
+	int idx = 0;
+	int multiply_by = 1;
+	for(int d = 0; d < dims; d++) {
+		idx += coords[d] * multiply_by;
+		multiply_by <<= bits; // multiply by N
+	}
+	return idx;
 }
 
 template<>
 double CahnHilliard<2>::cell_laplacian(std::vector<double> &field, int idx) {
-  int coords_xy[2];
-  fill_coords(coords_xy, idx);
+	int coords_xy[2];
+	fill_coords(coords_xy, idx);
 
-  int coords_xmy[2] = {
-			  (coords_xy[0] - 1 + N) & N_minus_one,
-			  coords_xy[1]
-  };
-  
-  int coords_xym[2] = {
-			  coords_xy[0],
-			  (coords_xy[1] - 1 + N) & N_minus_one
-  };
-  
-  int coords_xpy[2] = {
-			  (coords_xy[0] + 1) & N_minus_one,
-			  coords_xy[1]
-  };
-  
-  int coords_xyp[2] = {
-			  coords_xy[0],
-			  (coords_xy[1] + 1) & N_minus_one
-  };
+	int coords_xmy[2] = {
+			(coords_xy[0] - 1 + N) & N_minus_one,
+			coords_xy[1]
+	};
 
-  return (field[cell_idx(coords_xmy)] + field[cell_idx(coords_xpy)] + field[cell_idx(coords_xym)] + field[cell_idx(coords_xyp)] - 4 * field[idx]) / (H * H);
+	int coords_xym[2] = {
+			coords_xy[0],
+			(coords_xy[1] - 1 + N) & N_minus_one
+	};
+
+	int coords_xpy[2] = {
+			(coords_xy[0] + 1) & N_minus_one,
+			coords_xy[1]
+	};
+
+	int coords_xyp[2] = {
+			coords_xy[0],
+			(coords_xy[1] + 1) & N_minus_one
+	};
+
+	return (field[cell_idx(coords_xmy)] + field[cell_idx(coords_xpy)] + field[cell_idx(coords_xym)] + field[cell_idx(coords_xyp)] - 4 * field[idx]) / (H * H);
 }
 
 template<int dims>
 void CahnHilliard<dims>::evolve() {
-  static std::vector<double> free_energy_der(psi.size());
-  for(unsigned int idx = 0; idx < psi.size(); idx++) {
-    free_energy_der[idx] = cell_laplacian(psi, idx) + epsilon * psi[idx] - psi[idx] * psi[idx] * psi[idx];
-  }
+	static std::vector<double> free_energy_der(psi.size());
+	for(unsigned int idx = 0; idx < psi.size(); idx++) {
+		free_energy_der[idx] = k_laplacian * cell_laplacian(psi, idx) + epsilon * psi[idx] - psi[idx] * psi[idx] * psi[idx];
+	}
 
-  for(unsigned int idx = 0; idx < psi.size(); idx++) {
-    psi[idx] += -D * cell_laplacian(free_energy_der, idx) * DT;
-  }
+	for(unsigned int idx = 0; idx < psi.size(); idx++) {
+		psi[idx] += -D * cell_laplacian(free_energy_der, idx) * DT;
+	}
 }
 
 template<int dims>
 void CahnHilliard<dims>::print_state(std::string filename) {
-  std::ofstream out(filename.c_str());
-  
-  for(int idx = 0; idx < size; idx++) {
-    if(idx > 0) {
-      int modulo = N;
-      for(int d = 1; d < dims; d++) {
-	if(idx % modulo == 0) {
-	  out << std::endl;
-	}
-	modulo <<= N;
-      }
-    }
-    out << psi[idx] << " ";
-  }
+	std::ofstream out(filename.c_str());
 
-  out.close();
+	for(int idx = 0; idx < size; idx++) {
+		if(idx > 0) {
+			int modulo = N;
+			for(int d = 1; d < dims; d++) {
+				if(idx % modulo == 0) {
+					out << std::endl;
+				}
+				modulo <<= N;
+			}
+		}
+		out << psi[idx] << " ";
+	}
+
+	out.close();
 }
 
 int main(int argc, char *argv[]) {
-  srand48(51328);
+	srand48(51328);
 
-  if(argc < 5) {
-    fprintf(stderr, "Usage is %s N epsilon psi_average steps\n", argv[0]);
-    exit(1);
-  }
+	if(argc < 5) {
+		fprintf(stderr, "Usage is %s N epsilon psi_average steps [k=1]\n", argv[0]);
+		exit(1);
+	}
 
-  int N = std::atoi(argv[1]);
-  double epsilon = std::atof(argv[2]);
-  double psi_average = std::atof(argv[3]);
-  long long int steps = std::atol(argv[4]);
+	int N = std::atoi(argv[1]);
+	double epsilon = std::atof(argv[2]);
+	double psi_average = std::atof(argv[3]);
+	long long int steps = std::atol(argv[4]);
 
-  CahnHilliard<2> system(N, epsilon, psi_average);
-  
-  for(int t = 0; t < steps; t++) {
-    system.evolve();
-  }
+	double k = 1.0;
+	if(argc > 5) {
+		k = std::atof(argv[5]);
+	}
 
-  system.print_state("last.dat");
+	CahnHilliard<2> system(N, epsilon, psi_average, k);
 
-  return 0;
+	for(int t = 0; t < steps; t++) {
+		system.evolve();
+	}
+
+	system.print_state("last.dat");
+
+	return 0;
 }
