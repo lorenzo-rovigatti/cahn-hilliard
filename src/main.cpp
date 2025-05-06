d8c3bca8cbde8a3e3735567012b95a422d784d90
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2025-05-06 12:52:53 +0200

    Have the code parse the initial time step out of the initial configuration (if provided)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "models/RicciWertheim.h"
 #include "models/SalehWertheim.h"
 #include "models/SimpleWertheim.h"
+#include "utils/strings.h"
 
 #include <iostream>
 #include <fstream>
@@ -62,10 +63,34 @@ public:
 
 		_system = std::make_unique<ch::CahnHilliard<DIM>>(_model.get(), config);
 
+		if(config["load_from"]) {
+			_openmode = std::ios_base::app;
+			info("Restarting the simulation from a previous configuration: the output information and configurations will be appended to the respective files");
+			// we open the file and try to read out the time step 
+			std::string filename = _config_value<std::string>(config, "load_from");
+			std::ifstream load_from(filename.c_str());
+
+			// the CahnHilliard object has already parsed this file, so we can take for granted
+			// that it exists and it is well-formed
+			std::string line;
+			std::getline(load_from, line);
+			utils::trim(line);
+			auto spl = utils::split(line);
+			if(spl.size() > 3 && spl[0] == "#" && spl[1] == "step" && spl[2] == "=") {
+				_initial_t = std::stoll(spl[3]);
+				info("Initial time step (as parsed from the 'load_from' file): {}", _initial_t);
+			}
+			else {
+				warning("The header line of the 'load_from' file does not contain information about the time step", _initial_t);
+			}
+
+			load_from.close();
+		}
+
 		_trajectories.resize(_model->N_species());
 		if(_print_trajectory_every > 0 || _log_n0 > 0) {
 			for(int i = 0; i < _model->N_species(); i++) {
-				_trajectories[i].open(fmt::format("trajectory_{}.dat", i));
+				_trajectories[i].open(fmt::format("trajectory_{}.dat", i), _openmode);
 			}
 		}
 	}
@@ -81,20 +106,20 @@ public:
 	void run() {
 		_print_current_state("init_", 0);
 
-		std::ofstream mass_output("energy.dat");
+		std::ofstream mass_output("energy.dat", _openmode);
 
-		for(long long int t = 0; t < _steps; t++) {
-			if(_should_print_last(t)) {
-				_print_current_state("last_", t);
+		for(_t = _initial_t; _t < _initial_t + _steps; _t++) {
+			if(_should_print_last(_t)) {
+				_print_current_state("last_", _t);
 			}
-			if(_should_print_traj(t)) {
+			if(_should_print_traj(_t)) {
 				for(int i = 0; i < _model->N_species(); i++) {
-					_system->print_species_density(i, _trajectories[i], t);
+					_system->print_species_density(i, _trajectories[i], _t);
 				}
 				_traj_printed++;
 			}
-			if(_print_mass_every > 0 && t % _print_mass_every == 0) {
-				std::string output_line = fmt::format("{:.5} {:.8} {:.5} {:L}", t * _system->dt, _system->average_free_energy(), _system->average_mass(), t);
+			if(_print_mass_every > 0 && _t % _print_mass_every == 0) {
+				std::string output_line = fmt::format("{:.5} {:.8} {:.5} {:L}", _t * _system->dt, _system->average_free_energy(), _system->average_mass(), _t);
 				mass_output << output_line << std::endl;
 				std::cout << output_line << std::endl;
 			}
@@ -132,10 +157,13 @@ private:
 	}
 
 	std::string _print_traj_strategy;
+	long long int _initial_t = 0;
+	long long int _t;
 	long long int _steps, _print_mass_every, _print_trajectory_every, _print_last_every;
 	int _traj_printed = 0;
 	int _log_n0 = 0;
 	double _log_fact = 0;
+	std::ios_base::openmode _openmode = std::ios_base::out;
 
 	std::vector<std::ofstream> _trajectories;
 	std::unique_ptr<ch::FreeEnergyModel> _model;
